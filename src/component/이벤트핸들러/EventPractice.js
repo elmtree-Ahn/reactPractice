5560b8b797baa8f83abca2ea85809b90f8545a47
elmtree-Ahn <clfclf3138@gmail.com>
2021-10-10 17:18:13 +0900

    이벤트 처리 마무리

--- src/component/이벤트핸들러/EventPractice.js
+++ src/component/이벤트핸들러/EventPractice.js
@@ -1,22 +1,53 @@
 import { useEffect, useState } from 'react'
 
 function EventPractice() {
-  
-  const [inputData, setInputData] = useState('');
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUserName = e => {
+    setUsername(e.target.value);
+  }
+  const onChangeMessage = e => {
+    setMessage(e.target.value);
+  }
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setUsername('');
+    setMessage('');
+  }
 
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
+      onClick()
+    }
+  }
 
   return (
     <div>
-      <h1>이벤트 테스트</h1>
-      <input type="text" placeholder="내용을 입력하세요" onChange={(e) => {
-        setInputData(e.target.value);
-      }}/>
-      <button onClick={() => {
-        alert(inputData);
-      }}>확인</button>
+      <h1>이벤트 연습</h1>
+      {/* 사용자명 입력 */}
+      <input 
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUserName}
+      />
+      {/* 메세지 입력 */}
+      <input 
+        type="text"
+        name="message"
+        placeholder="내용을 입력하세요"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button
+        onClick={onClick}>
+        확인
+      </button>
     </div>
+
   )
-  
 }
 
 export default EventPractice
